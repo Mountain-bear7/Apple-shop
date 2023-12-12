9c069e4385e9165760dacb9644d841db405f7b33
Mountain-bear7 <doluxan010406@gmail.com>
2023-12-12 10:41:00 +0300

    first commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=1235-20_course-work',
+    'username' => '1235-20_bear',
+    'password' => 'Mountain-bear7',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
